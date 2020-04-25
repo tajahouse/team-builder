8274c31d982fc819ad29fb660ef1c3de7bc403b2
Taja House <ahlliahtaja@yahoo.com>
2020-04-25 12:01:18 -0700

    Created a list and rendered it on the page for initialState

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const names = [
+  "Laura",
+  "Sidney",
+  "Derwin",
+  "Taja",
+  "Angelia",
+  "Lazurus",
+  "Jucobie",
+];
+
 function App() {
   const[teamMember, setTeamMember] = useState({name: ""});
+  const[teamList, setTeamList] = useState(names);
 
   return (
     <div className="App">
@@ -11,6 +22,13 @@ function App() {
       <label htmlFor ="name">Name</label>
       <input id="name" type="text" placeholder="Team member name"/>
     </form>
+    <div>
+      <ul>
+        {teamList.map(name =>{
+          return <li key={name}>{name}</li>;
+        })}
+      </ul>
+    </div>
     </div>
   );
 }
