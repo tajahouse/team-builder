2ba738f37dc80aab539024aab105e61018ee86c7
Taja House <ahlliahtaja@yahoo.com>
2020-04-25 12:54:11 -0700

    Added an onChange to my state and also created a button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const names = [
-  "Laura",
-  "Sidney",
-  "Derwin",
-  "Taja",
-  "Angelia",
-  "Lazurus",
-  "Jucobie",
-];
+  const names = [
+    "Laura",
+    "Sidney",
+    "Derwin",
+    "Taja",
+    "Angelia",
+    "Lazurus",
+    "Jucobie",
+  ];
 
 function App() {
   const[teamMember, setTeamMember] = useState({name: ""});
   const[teamList, setTeamList] = useState(names);
+  const handleChanges = event =>{
+    setTeamMember({ name: event.target.value });
+    console.log({event});
+  };
+  const addNewName = member =>{
+    const newName ={
+      id:Date.now(),
+      name: member.name
+    };
+    setTeamMember([...teamMember, newName])
+  }
+
+  const submitForm = event => {
+    event.preventDefault();
+    addNewName(teamMember);
+  };
+
 
   return (
     <div className="App">
-    <form>
+    <form onSubmit ={submitForm}>
       <label htmlFor ="name">Name</label>
-      <input id="name" type="text" placeholder="Team member name"/>
+      <input 
+        id="name" 
+        onChange={handleChanges}
+        type="text" 
+        placeholder="Team member name"
+      />
+      <button type="submit">Add Name</button>
     </form>
     <div>
       <ul>
